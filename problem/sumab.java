2fdd98ba7ba015dac758d77c33fdfb15dbfec8ba
andrew <1124913956@qq.com>
2019-02-28 18:02:09 +0800

    1. A + B

---
+++ problem/sumab.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class sumab {
+	 public static void main(String args[]) {
+	        Scanner scan = new Scanner(System.in);
+	        //String line;
+	        while (scan.hasNext()) {
+	            //line = scan.nextLine().trim();
+	            // please write your code here
+	            long a = scan.nextLong();
+	            long b = scan.nextLong();
+	            // System.out.println("answer");
+	            System.out.println(a+b);
+	        }
+	    }
+}
